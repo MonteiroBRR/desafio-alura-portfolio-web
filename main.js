6c03b64ad010d457fccbc91e51b9845001d66bd0
Rafael Monteiro <114002447+MonteiroBRR@users.noreply.github.com>
2023-05-22 16:05:47 -0300

    foi criado a interação com os ícones interesses.
    Também foi feita a refatoração de uma parte de código para melhorar a manutenção.

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
-function mostraMenu(){
+function mostraMenuMobile() {
     let menuMobile = document.querySelector('.menu_mobile');
-    if (menuMobile.classList.contains('revela')){
-        menuMobile.classList.remove('revela');
-        document.querySelector('.icone').src="assets/menuMobile.svg"
-    } else{
-        menuMobile.classList.add('revela')
-        document.querySelector('.icone').src="assets/close.svg"
+    if (menuMobile.classList.contains('revelaElemento')) {
+        menuMobile.classList.remove('revelaElemento');
+        document.querySelector('.icone').src = "assets/menuMobile.svg"
+    } else {
+        menuMobile.classList.add('revelaElemento')
+        document.querySelector('.icone').src = "assets/close.svg"
 
     }
 }
 
-function mostraTexto(){
-    let textoInteresses = document.querySelector('.texto_interesses');
-    textoInteresses.innerHTML = 'Passar o texto que queremos nessa string'
+document.querySelector('.botaoMenuMobile').onclick = mostraMenuMobile;
+
+//---------------------------------------
+const listaDeIconesInteresses = document.querySelectorAll('.icone_interesses')
+
+for(let i = 0; i < listaDeIconesInteresses.length; i++) {
+    const INTERESSE = listaDeIconesInteresses[i]
+    let idInteresse = INTERESSE.id
+    
+    INTERESSE.onclick = function() {
+        mostraTexto(idInteresse)
+    }
+}
+
+const textoInteresses = {
+    'interesse_html':'O html estrutura a página da web',
+    'interesse_css':'O css estiliza a página da web',
+    'interesse_javascript':'É a linguagem de programação que permite dinamismo e interatividade',
+    'interesse_git':'É um controlador de versões de projetos'
+}
+
+function mostraTexto (idInteresse) {
+    let texto = document.querySelector('#texto-interesses')
+    let caixaDisplay = document.querySelector('#caixa-interesses')
+
+    caixaDisplay.classList.remove('caixa_interesses')
+
+    texto.innerHTML = textoInteresses[idInteresse] || ''
 }
